c4311f1eb540debebe2dc9485dea67bf9d6c432f
TechJeeper <caffeinegamer@gmail.com>
2025-11-30 15:19:36 -0500

    Delete PandaCam directory

--- PandaCam/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('pandacam', {
-  // Functions to communicate with the main process
-  selectSdpFile: () => ipcRenderer.invoke('select-sdp-file'),
-  startWebcam: (sdpFilePath) => ipcRenderer.invoke('start-webcam', sdpFilePath),
-  stopWebcam: () => ipcRenderer.invoke('stop-webcam')
-}); 
