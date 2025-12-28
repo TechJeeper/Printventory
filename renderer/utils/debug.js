23f4cdbe0d9a2b08aa97703f5c06ab2cd0e96e90
TechJeeper <caffeinegamer@gmail.com>
2025-12-28 08:07:17 -0500

    Cleanup

--- renderer/utils/debug.js
+++
@@ -1,7 +0,0 @@
-// utils/debug.js
-function debugLog(...args) {
-  console.log('Hello World...');
-  if (window.constants?.DEBUG) {
-    console.log('[DEBUG]', ...args);
-  }
-}
--- renderer/utils/formatFileSize.js
+++
@@ -1,5 +0,0 @@
-function formatFileSize(bytes) {
-  if (bytes >= 1_000_000) return (bytes / 1_000_000).toFixed(2) + ' MB';
-  if (bytes >= 1_000) return (bytes / 1_000).toFixed(1) + ' KB';
-  return bytes + ' bytes';
-}
